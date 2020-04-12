6e7a18a78c8f5bb1e9842ce7e8bf958d57272142
KGC-ISU <overnice2020@gmail.com>
2020-04-12 22:05:23 +0900

    로그인 프로세스 구현

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="https://kit.fontawesome.com/bf93393bff.js" crossorigin="anonymous"></script>
+    <script src="./js/login.js"></script>
+    
+    <link rel="stylesheet" href="./css/login.css">    
+
+    <title>오늘의 단어</title>    
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <div class="clock"></div>
+        </div>
+    </div>
+</body>
+</html>

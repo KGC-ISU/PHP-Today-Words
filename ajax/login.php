c92f333b25d34a04045ebfe00db3e1a524fc4dfc
KGC-ISU <overnice2020@gmail.com>
2020-06-01 22:06:45 +0900

    로그인 테이블 변경에 따른 코드 수정

--- ajax/login.php
+++ ajax/login.php
@@ -1,18 +1,23 @@
 <?php
-    include_once "../include/db.php";
+    include_once "../include/db_connect.php";
+
+    $conn = db_connect();
 
     $name = $_POST['name'];
 
-    $query = "SELECT * FROM member WHERE name = '$name'";
-    $rs = $mysqli->query($query);
-    $cnt = mysqli_num_rows($rs);
+    $query = "SELECT COUNT(seq) as cnt FROM member WHERE user_name = '$name'";
+    $rs = mysqli_query($conn, $query);
+    $row = mysqli_fetch_array($rs);
+
+    $cnt = $row['cnt'];
+
     if ($cnt < 1) {
         $insert = "
             INSERT INTO member SET
-                name = '".$name."',
-                modt = '".date("Y-m-d H:i:s")."'
+                user_name = '".$name."',
+                create_dt = '".date("Y-m-d H:i:s")."'
         ";
-        if(!$mysqli->query($insert)) {
+        if(!mysqli_query($conn, $insert)) {
             $res = array("status" => false, "msg" => "새로운 정보 추가 중 오류가 발생했습니다!", "icon" => "error", "title" => "이런!");
             echo json_encode($res);
             exit;
@@ -22,4 +27,3 @@
     $res = array("status" => true, "msg" => "환영합니다! ".$name."님!", "icon" => "success", "title" => "성공!");
     echo json_encode($res);
     exit;
-?>

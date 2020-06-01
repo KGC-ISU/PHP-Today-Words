9ec06d4fe8be3ede0b13eae5eaab4e3c5b891cb0
KGC-ISU <overnice2020@gmail.com>
2020-06-01 23:25:35 +0900

    저장된 단어 검색 후 없을 시 파싱 후 저장 프로세스 구현

--- main.php
+++ main.php
@@ -1,19 +1,24 @@
 <?php
-    include_once("./lib/simple_html_dom.php");
-    include_once("./include/function.php");
+    include_once("./include/db_connect.php");
+    include_once("./lib/simple_html_dom.php"); 
+    include_once("./include/function.php");    
 
-    $url = "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EC%98%A4%EB%8A%98%EC%9D%98+%EB%8B%A8%EC%96%B4";
-    $html = file_get_html($url);
-    $elements = $html->find('ul.word_lst', 0)->find('li');
+    $conn = db_connect();
 
-    $word_arr = array();
-    foreach($elements as $el) {
-        $key = $el->find('a.word', 0)->plaintext;
-        $value = $el->find('span.mean', 0)->plaintext;
-        $value = explode(';', $value);
-        $word_arr[$key] = $value;
+    $nowdt = date("Y-m-d");
+    $query = "SELECT COUNT(*) AS 'cnt' FROM today_word WHERE modt = '$nowdt'";
+    $row = mysqli_fetch_array( mysqli_query($conn, $query) );
+
+    if ($row['cnt'] < 1) {
+        $date = date("Y.m.d");
+        $url = "https://learn.dict.naver.com/m/endic/today/words.nhn?targetDate=".$date;
+        $data = file_get_html($url);
+
+        $html = new simple_html_dom();        
+
+        setTodayWords($conn, $data, $html);
     }
-
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,23 +34,6 @@
     <script src="./js/main.js"></script>
 </head>
 <body>
-    <div id="outer">
-        <div id="background-color"></div>
-        <div class="container">
-            <div class="word-line">
-                <? foreach ($word_arr as $eng => $word) { ?>
-                    <div class="box">
-                        <span class="word-title"><?= $eng ?></span>
-                        <? foreach ($word as $value) { ?>
-                            <p class="word-text"><?= $value ?></p>                        
-                        <? } ?>
-                    </div>
-                <? } ?>
-                <div class="box">
-                    
-                </div>
-            </div>
-        </div>
-    </div>    
+ 
 </body>
 </html>

aafb2227ffc8366e5f8e67abcb9b77fcd7069d38
KGC-ISU <overnice2020@gmail.com>
2020-06-01 23:24:38 +0900

    JS 초기화

--- js/main.js
+++ js/main.js
@@ -3,13 +3,3 @@ const background_arr = [
     //"linear-gradient(to top, #bdc2e8 0%, #bdc2e8 1%, #e6dee9 60%)",        // Marble Wall
     "linear-gradient(to top, #dad4ec 0%, #dad4ec 1%, #f3e7e9 100%)"         // Confident Cloud
 ];
-
-$(function() {
-    pageLoading();
-});
-
-function pageLoading() {
-    var num = Math.floor((Math.random() * background_arr.length));
-    console.log(num);
-    $("#background-color").css("background-image", background_arr[num]).fadeIn(1000);
-}

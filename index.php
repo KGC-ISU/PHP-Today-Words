406157ea5e79451c456663a345a256096dfdc4bc
KGC-ISU <overnice2020@gmail.com>
2020-06-03 22:26:09 +0900

    백그라운드 색상 설정

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script src="https://kit.fontawesome.com/bf93393bff.js" crossorigin="anonymous"></script>
-    <script src="./js/login.js"></script>
     
-    <link rel="stylesheet" href="./css/login.css">    
+    <link rel="stylesheet" href="./css/style.css">    
 
     <title>오늘의 단어</title>    
 </head>
 <body>
-    
+    <div id="background-color"></div>
 </body>
 </html>
